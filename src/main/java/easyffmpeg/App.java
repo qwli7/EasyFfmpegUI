2260562938e0a031af37e503350e3ecec5d37217
qwli7 <228794754@qq.com>
2025-07-04 11:12:19 +0800

    Update App.java

--- src/main/java/easyffmpeg/App.java
+++ src/main/java/easyffmpeg/App.java
@@ -1,6 +1,9 @@
 package easyffmpeg;
 
+import easyffmpeg.ui.MainPane;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
@@ -21,24 +24,29 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("EasyFfmpegUI");
-        Button button = new Button("浏览");
+//        Button button = new Button("浏览");
+//
+//        button.setOnAction(event -> {
+//            FileChooser fileChooser = new FileChooser();
+//            fileChooser.setTitle("选择文件");
+//            fileChooser.setInitialDirectory(new File(".")); //初始路径
+//
+//            fileChooser.getExtensionFilters().addAll(
+//                    new FileChooser.ExtensionFilter("图片", "*.jpg", "*.png"),
+//                    new FileChooser.ExtensionFilter("视频", "*.mp4"),
+//                    new FileChooser.ExtensionFilter("所有文件", "*.*")
+//            );
+//
+//            File file = fileChooser.showOpenDialog(stage);
+//            System.out.println(file.toString());
+//        });
 
-        button.setOnAction(event -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("选择文件");
-            fileChooser.setInitialDirectory(new File(".")); //初始路径
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(this.getClass().getResourceAsStream("/fxml/MainPane.fxml"));
+        Object controller = fxmlLoader.getController(); //获取到对应的控制器
 
-            fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("图片", "*.jpg", "*.png"),
-                    new FileChooser.ExtensionFilter("视频", "*.mp4"),
-                    new FileChooser.ExtensionFilter("所有文件", "*.*")
-            );
-
-            File file = fileChooser.showOpenDialog(stage);
-            System.out.println(file.toString());
-        });
-
-        Scene scene = new Scene(button, initWindow, initHeight);
+//        MainPane mainPane = new MainPane(stage);
+        Scene scene = new Scene(root, initWindow, initHeight);
         stage.setScene(scene);
         stage.show();
     }
